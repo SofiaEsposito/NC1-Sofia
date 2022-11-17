624d97a5cdda66d5b543cc1f31ce2533a07b71b0
SofiaEsposito <sesposito22@fed.idserve.net>
2022-11-17 17:31:46 +0100

    Start point

---
+++ NC1/NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Sofia Esposito on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
